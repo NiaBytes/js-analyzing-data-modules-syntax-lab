601a53f78a918b00e912c92dd48b0fed3b04be68
NiaBytes <180058599+NiaBytes@users.noreply.github.com>
2026-08-14 21:29:26 -0500

    done

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+require('datejs');
 
+function combineUsers(...args) {
+  let combinedObject = {
+    users: []
+  };
 
+  for (let i = 0; i < args.length; i++) {
+    combinedObject.users = [...combinedObject.users, ...args[i]];
+  }
+
+  combinedObject.merge_date = new Date().toString('M/d/yyyy');
+
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
